768c572feeb037c34a4cc93327c02aaafd616f65
orbxball <10207726+orbxball@users.noreply.github.com>
2021-03-19 16:37:27 -0400

    support crv.finance: Curve.fi Factory USD Metapool v2

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -74,6 +74,13 @@ interface ICurveFi {
         uint256[4] calldata amounts,
         uint256 min_mint_amount
     ) external payable;
+
+    // crv.finance: Curve.fi Factory USD Metapool v2
+    function add_liquidity(
+        address pool,
+        uint256[4] calldata amounts,
+        uint256 min_mint_amount
+    ) external;
 }
 
 interface IVoterProxy {
